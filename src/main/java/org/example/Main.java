b65af7595368845fe0231815550a4b46a251157d
Leo <leocho1126@gmail.com>
2023-09-01 16:52:21 +0900

    Create Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("219000", "홍길동");
+
+        System.out.println("Hello world!");
+
+    }
+
+}
